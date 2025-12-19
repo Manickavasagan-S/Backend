86671f57c37c49a54fc2a3770c904f479ac33b1d
manickavasagan-s <manickavasagan.s2024lcse@sece.ac.in>
2025-12-19 11:50:44 +0530

    Fixing proplem 2

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ const mongoose = require('mongoose');
 dotenv.config({ path: require('path').join(__dirname, 'config.env') });
 
 mongoose
-  .connect(process.env.DB_URL)
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 30000,
+    socketTimeoutMS: 45000,
+    bufferMaxEntries: 0
+  })
   .then(() => {
     console.log('MongoDB connected to TaskManagementSystem');
   })
